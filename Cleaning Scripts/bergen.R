54b410552d6f3793c2abdce1314a6f7459bc11a1
RMalhotraGit <rm1229@scarletmail.rutgers.edu>
2020-05-02 18:56:12 -0400

    Added cases data for each city/town in Bergen County, NJ

---
+++ Cleaning Scripts/bergen.R
@@ -0,0 +1,30 @@
+library(rvest)
+library(tidyr)
+library(dplyr)
+
+# scraping bergen county data
+url <- "https://www.insidernj.com/bergen-county-town-covid-19-list-15982-cases-total-friday/"
+bergen <- url %>%
+  read_html() %>%
+  html_nodes("p") %>%
+  html_text()
+
+# to find starting index, we look for Allendale, since list is in alphabetical order
+startIndex <- 0
+for (i in 1:length(bergen)) {
+  if(grepl("*Allendale", bergen[i])) {
+    startIndex <- i
+  }
+}
+# add on 69 since there are a total of 70 cities/towns in Bergen county
+endIndex <- startIndex + 69
+
+# save just the cities/towns and cases from html text
+bergen <- bergen[startIndex:endIndex]
+
+# separate city/town and the number of cases
+bergen <- separate(as.data.frame(bergen), bergen, c("City/Town", "Cases"), sep = ":")
+
+# remove * that appears in some of the city/town names
+bergen$`City/Town` <- bergen$`City/Town` %>%
+  str_replace_all("\\*", "")
