27e31279e72ef2b492ee65044ce8a8e4c22a6fef
RMalhotraGit <rm1229@scarletmail.rutgers.edu>
2020-05-02 15:35:25 -0400

    Scraping and initial cleaning of NJ county data

---
+++ nj.R
@@ -0,0 +1,16 @@
+library(rvest)
+
+# scraping nj data
+nj_url <- "https://www.worldometers.info/coronavirus/usa/new-jersey/"
+nj <- nj_url %>%
+  read_html() %>%
+  html_table(fill = TRUE) %>%
+  .[[1]]
+
+# getting rid of of column for total tests in NJ and source column
+nj <- nj[, !names(nj) %in% c("TotalTests", "Source")]
+
+# getting rid of rows that are not counties
+nj <- nj[(nj$County != "New Jersey Total") &
+           (nj$County != "Unassigned") &
+           (nj$County != "Total:"),]
