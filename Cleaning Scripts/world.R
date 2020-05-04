8fc147534cb783e6d4dd7b400b7973c2190c5b35
RMalhotraGit <rm1229@scarletmail.rutgers.edu>
2020-05-03 22:00:23 -0400

    Made a modified version of world data in order to create choroplethr maps

--- Cleaning Scripts/world.R
+++ Cleaning Scripts/world.R
@@ -32,3 +32,25 @@ world[is.na(world)] <- 0
 
 # reassinging id values to account for removed rows
 rownames(world) <- seq(1, dim(world)[1])
+
+### CHOROPLETHR VERSION OF DATA
+# creating a copy of the dataset to use for choroplethr
+world.choro <- world
+
+# change to lower case
+world.choro$`Country,Other` <- tolower(world.choro$`Country,Other`)
+
+# change country names to match those in choroplethr
+world.choro$`Country,Other`[world.choro$`Country,Other` == "usa"] <- "united states of america"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "uk"] <- "united kingdom"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "s. koera"] <- "south korea"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "czechia"] <- "czech republic"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "serbia"] <- "republic of serbia"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "uae"] <- "united arab emirates"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "north macedonia"] <- "macedonia"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "timor-leste"] <- "east timor"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "tanzania"] <- "united republic of tanzania"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "bahamas"] <- "the bahamas"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "drc"] <- "democratic republic of the congo"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "congo"] <- "republic of congo"
+world.choro$`Country,Other`[world.choro$`Country,Other` == "guinea-bissau"] <- "guinea bissau"
