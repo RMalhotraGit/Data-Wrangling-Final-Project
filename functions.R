3597ffd90182534d37237b73465496571a609c69
RMalhotraGit <rm1229@scarletmail.rutgers.edu>
2020-05-04 23:29:06 -0400

    Created a linear regression model to predict the number of cases using the number of tests and did a similar comparison between all countries and the United States (as done for predicting deaths by the cases)

--- functions.R
+++ functions.R
@@ -15,3 +15,10 @@ lm.deaths <- function(df) {
   model <- lm(TotalDeaths ~ TotalCases, data = df)
   return(model)
 }
+
+# function to generate a basic linear model for predicting number of cases based on number of tests
+# (assumes the dataframe has 'TotalCases' and 'TotalTests' variables)
+lm.cases <- function(df) {
+  model <- lm(TotalCases ~ TotalTests, data = df)
+  return(model)
+}
